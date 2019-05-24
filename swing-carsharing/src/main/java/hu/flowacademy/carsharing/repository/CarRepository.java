108d5df4adc7697e904aef12fe129795e176d9b6
wunderszindi <sandrine.varga@gmail.com>
2019-05-24 09:34:02 +0200

    Remove duplicated directory

--- swing-carsharing/src/main/java/hu/flowacademy/carsharing/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.flowacademy.carsharing.repository;
-
-import hu.flowacademy.carsharing.domain.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<Car, String> {
-}
--- swing-carsharing/src/main/java/hu/flowacademy/carsharing/repository/DriverRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.flowacademy.carsharing.repository;
-
-import hu.flowacademy.carsharing.domain.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DriverRepository extends JpaRepository<Driver, String> {
-}
